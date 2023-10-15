842eaf1f276613e5be30247f72bdd7f6191e7acf
hrndzValentin <vaher.246@gmail.com>
2023-10-14 19:11:57 -0500

    tercer commit

--- Ejercicio4.8/src/main/java/org/carreraciclistica/Equipo.java
+++ Ejercicio4.8/src/main/java/org/carreraciclistica/Equipo.java
@@ -7,59 +7,70 @@ public class Equipo {
     private String país;
     Vector listaCiclistas;
 
-    public Equipo(String nombre, String país) {
-        this.nombre = nombre;
-        this.país = país;
-        totalTiempo = 0;
-        listaCiclistas = new Vector();
-
     public String getNombre() {
-        return this.nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    private String getPaís() {
-        return this.país;
+    public String getPaís() {
+        return país;
     }
 
-    private void setPaís(String país) {
+    public void setPaís(String país) {
         this.país = país;
     }
 
-    void añadirCiclista(Ciclista ciclista) {
+    public Equipo(String nombre, String país) {
+        this.nombre = nombre;
+        this.país = país;
+        totalTiempo = 0;
+        listaCiclistas = new Vector();
+
+    }
+
+        void añadirCiclista (Ciclista ciclista){
             listaCiclistas.add(ciclista);
-    }
+        }
 
-void listarEquipo() {
-    for (int i = 0; i < listaCiclistas.size(); i++) {
-        Ciclista c = (Ciclista) listaCiclistas.elementAt(i);
-        System.out.println(c.getNombre());
-    }
-}
-
-    void buscarCiclista() {
-        Scanner sc = new Scanner(System.in);
-        String nombreCiclista = sc.next();
-        for (int i = 0; i < listaCiclistas.size(); i++) {
-            Ciclista c = (Ciclista) listaCiclistas.elementAt(i);
-            if (c.getNombre().equals(nombreCiclista)) {
+        void listarEquipo () {
+            for (int i = 0; i < listaCiclistas.size(); i++) {
+                Ciclista c = (Ciclista) listaCiclistas.elementAt(i);
                 System.out.println(c.getNombre());
             }
         }
-    }
 
-    public void añadir() {
-        for (int i = 0; i < listaCiclistas.size(); i++) {
-            Ciclista c = (Ciclista) listaCiclistas.elementAt(i);
+        void buscarCiclista () {
+            Scanner sc = new Scanner(System.in);
+            String nombreCiclista = sc.next();
+            for (int i = 0; i < listaCiclistas.size(); i++) {
+                Ciclista c = (Ciclista) listaCiclistas.elementAt(i);
+                if (c.getNombre().equals(nombreCiclista)) {
+                    System.out.println(c.getNombre());
+                }
+            }
+        }
+
+        public void añadir () {
+            for (int i = 0; i < listaCiclistas.size(); i++) {
+                Ciclista c = (Ciclista) listaCiclistas.elementAt(i);
+                totalTiempo = totalTiempo + c.getTiempoAcumulado();
+            }
+        }
+        void imprimir () {
+            System.out.println("Nombre del equipo = " + nombre);
+            System.out.println("País = " + país);
+            System.out.println("Total tiempo del equipo = " + totalTiempo);
+        }
+    void calcularTotalTiempo() {
+        for (int i = 0; i < listaCiclistas.size(); i++) { // Se recorre el vector
+            Ciclista c = (Ciclista) listaCiclistas.elementAt(i); /* Se obtiene
+un elemento del vector */
+// Se acumula el tiempo del ciclista en el tiempo del equipo
             totalTiempo = totalTiempo + c.getTiempoAcumulado();
         }
     }
-    void imprimir() {
-        System.out.println("Nombre del equipo = " + nombre);
-        System.out.println("País = " + país);
-        System.out.println("Total tiempo del equipo = " + totalTiempo);
-    }
 }
+
